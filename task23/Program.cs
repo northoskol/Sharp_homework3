7691f1c3286e2d20f14fd3586cd9cd1cc9c96d8e
Dmitry <northoskol@gmail.com>
2023-07-11 17:35:12 +0300

    end task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void Square(int value)
+{
+    for (int i = 1; i <= value; i++)
+    {
+        int square = i * i * i;
+        Console.Write(square + " ");
+    }
+}
+
+int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int value = GetInput("Введите число: ");
+Square(value);
